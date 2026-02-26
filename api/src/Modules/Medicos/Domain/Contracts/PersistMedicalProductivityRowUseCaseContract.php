4039ce28c956de48aca7e59ac25810f9dc4e2a98
Mamura <marcio.mota@gmail.com>
2026-02-26 09:46:32 -0300

    contratos
    Refs #12

--- api/src/Modules/Medicos/Domain/Contracts/PersistMedicalProductivityRowUseCaseContract.php
+++ api/src/Modules/Medicos/Domain/Contracts/PersistMedicalProductivityRowUseCaseContract.php
@@ -3,8 +3,9 @@
 namespace App\Modules\Medicos\Domain\Contracts;
 
 use App\Modules\Medicos\Application\DTO\ParsedMedicalProductivityRowDTO;
+use App\Modules\Medicos\Application\DTO\PersistRowResultDTO;
 
 interface PersistMedicalProductivityRowUseCaseContract
 {
-    public function handle(ParsedMedicalProductivityRowDTO $row): void;
+    public function handle(ParsedMedicalProductivityRowDTO $row): PersistRowResultDTO;
 }
